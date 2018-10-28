f21290739298a730cd86f91ce34fc58abdda88c2
dwkns <dwkns@me.com>
2018-10-28 12:15:06 +0000

    Updated Sublime config

--- settings/test.py
+++ settings/test.py
@@ -1,17 +1,17 @@
-import sublime, sublime_plugin
+# import sublime, sublime_plugin
 
-def do_window_thing():
-    allWindows = sublime.windows()    
-    currentWindow = sublime.active_window()
-    for index,window in enumerate(allWindows):
-        window.run_command("set_layout", { "cols": [0.0, 0.5, 1.0 ], "rows": [0.0, 0.5, 1.0 ], "cells": [ [0, 0, 1, 2],[1, 0, 2, 1],[1, 1, 2, 2]]})
-        # window.run_command("terminal_view_open")
+# def do_window_thing():
+#     allWindows = sublime.windows()    
+#     currentWindow = sublime.active_window()
+#     for index,window in enumerate(allWindows):
+#         window.run_command("set_layout", { "cols": [0.0, 0.5, 1.0 ], "rows": [0.0, 0.5, 1.0 ], "cells": [ [0, 0, 1, 2],[1, 0, 2, 1],[1, 1, 2, 2]]})
+#         # window.run_command("terminal_view_open")
 
-def plugin_loaded():
-    do_window_thing()
-    print("----plugin loaded now----")
+# # def plugin_loaded():
+# #     do_window_thing()
+# #     print("----plugin loaded now----")
 
-class DoSomethingCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        print("----do something called tim----")
+# # class DoSomethingCommand(sublime_plugin.TextCommand):
+# #     def run(self, edit):
+# #         print("----do something called tim----")
 
